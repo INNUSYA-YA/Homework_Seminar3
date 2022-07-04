8073c002e7edfe6858e16f1a3d94095ca345cf0c
Inna <innasilivonchik@mail.ru>
2022-07-04 22:50:41 +0300

    Задача 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = InputInt("Введите число ");
+int minNumber = 1;
+
+while (minNumber <= number )
+{
+    int n = minNumber*minNumber*minNumber;
+    Console.Write(n + " ");
+    minNumber++;
+}
+
+
+
+
+
+int InputInt (string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
