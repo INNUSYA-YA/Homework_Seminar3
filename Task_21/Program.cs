9f2597d6e45b22321c67f9822ea5ed6dfe779973
Inna <innasilivonchik@mail.ru>
2022-07-04 21:08:54 +0300

    Задача 21

---
+++ Task_21/Program.cs
@@ -0,0 +1,21 @@
+﻿   int xOne = InputInt("Введите X1: ");
+   int yOne = InputInt("Введите Y1: ");
+   int zOne = InputInt("Введите Z1: ");
+   int xTwo = InputInt("Введите X2: ");
+   int yTwo = InputInt("Введите Y2: ");
+   int zTwo = InputInt("Введите Z1: ");
+   
+
+int firstSegment = xTwo - xOne;
+int secondSegment = yTwo - yOne;
+int ThirdSegment = zTwo - zOne;
+int sumQuadro = firstSegment*firstSegment+secondSegment*secondSegment+ThirdSegment*ThirdSegment;
+double sqrt = Math.Sqrt(sumQuadro);
+
+Console.WriteLine("Расстояние между точками " + sqrt);
+
+int InputInt (string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
