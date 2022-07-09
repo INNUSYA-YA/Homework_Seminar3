5e31d7659eea9c968b1cbfe1410261adc9eb4198
Inna <innasilivonchik@mail.ru>
2022-07-09 13:10:58 +0300

    Задача 19 испр

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,24 +1,11 @@
 ﻿int number = InputInt("Введите пятизначное число ");
 int n1 = number/10000;
 int n5 = number%10;
-int number1 = number;
-int count = 0;
-int n2 = 0;
-int n4 = 0;
 
-while(number>0)
-{
-number/=10;
-count++;
-if(count==3)
-{
-    n2 = number%10;
-}
-if(count==1)
-{
-    n4 = number%10;
-}
-}
+int n2 = (number/1000)%10;
+int n4 = (number/10)%10;;
+
+
 if(n1==n5 && n2==n4)
 {
     Console.WriteLine("Число является палиндромом");
